aed3d1400e563c911b6f3011480b4d94b35dd780
Arity-T <tish.mainmail@gmail.com>
2024-11-10 19:23:11 +0300

    Выводит относительный путь до файла

--- lab1/src/main/java/ru/spbstu/telematics/java/App.java
+++ lab1/src/main/java/ru/spbstu/telematics/java/App.java
@@ -6,20 +6,24 @@ public class App {
     public static void main(String[] args) {
         String path = (args.length > 0) ? args[0] : ".";
 
-        File directory = new File(path);
+        File filePath = new File(path);
 
-        if (directory.exists() && directory.isDirectory()) {
-            File[] files = directory.listFiles();
+        if (filePath.exists()) {
+            if (filePath.isFile()) {
+                System.out.println(filePath.getPath());
+            } else if (filePath.isDirectory()) {
+                File[] files = filePath.listFiles();
 
-            if (files != null) {
-                for (File file : files) {
-                    System.out.println(file.getName());
+                if (files != null) {
+                    for (File file : files) {
+                        System.out.println(file.getName());
+                    }
+                } else {
+                    System.out.println("Ошибка при чтении содержимого директории.");
                 }
-            } else {
-                System.out.println("Ошибка при чтении содержимого директории.");
             }
         } else {
-            System.out.println("Директория не существует или путь указан неверно.");
+            System.out.println("Директория или файл не существует или путь указан неверно.");
         }
     }
 }
