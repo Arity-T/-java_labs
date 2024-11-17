b5506a2aca3415239c540d0066d033942008a289
Arity-T <tish.mainmail@gmail.com>
2024-11-17 13:26:17 +0300

    Вынес получение списка файлов в отдельный метод

--- lab1/src/main/java/ru/spbstu/telematics/java/App.java
+++ lab1/src/main/java/ru/spbstu/telematics/java/App.java
@@ -1,29 +1,40 @@
 package ru.spbstu.telematics.java;
 
 import java.io.File;
+import java.util.List;
+import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) {
         String path = (args.length > 0) ? args[0] : ".";
+        for (String result : listFiles(path)) {
+            System.out.println(result);
+        }
+    }
 
+    public static List<String> listFiles(String path) {
         File filePath = new File(path);
+        List<String> results = new ArrayList<>();
 
         if (filePath.exists()) {
             if (filePath.isFile()) {
-                System.out.println(filePath.getPath());
+                results.add(filePath.getPath());
             } else if (filePath.isDirectory()) {
                 File[] files = filePath.listFiles();
 
                 if (files != null) {
                     for (File file : files) {
-                        System.out.println(file.getName());
+                        results.add(file.getName());
                     }
                 } else {
-                    System.out.println("Ошибка при чтении содержимого директории.");
+                    results.add("Ошибка при чтении содержимого директории.");
                 }
             }
         } else {
-            System.out.println("Директория или файл не существует или путь указан неверно.");
+            results.add("Директория или файл не существует или путь указан неверно.");
         }
+
+        return results;
     }
 }
+
