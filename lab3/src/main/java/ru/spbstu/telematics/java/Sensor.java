d8a6c256d5d207d6aa17c7d673febd298369ef1b
Arity-T <tish.mainmail@gmail.com>
2025-02-11 00:48:08 +0300

    Логика симуляции сенсоров температуры и влажности

--- lab3/src/main/java/ru/spbstu/telematics/java/Sensor.java
+++ lab3/src/main/java/ru/spbstu/telematics/java/Sensor.java
@@ -1,15 +1,50 @@
 package ru.spbstu.telematics.java;
 
+import java.util.Random;
+
 /* 
  * Симулирует работу сенсоров, установленных в комнате.
  */
 public class Sensor implements Runnable {
-    Room room;
-    private double temperature;
-    private double humidity;
+    private Room room;
+
+    public Sensor(Room room) {
+        this.room = room;
+    }
+    
+    // Температура
+    private volatile double temperature;
+
+    public double getTemperature() {
+		return temperature;
+	}
+
+    // Влажность
+	private volatile double humidity;
+
+    public double getHumidity() {
+		return humidity;
+	}
+
+	// Частота считывания значений с сенсоров
+    private long updateIntervalMs = 1000;
+
+    // Параметры произвольной ошибки измерений сенсоров
+    private Random random = new Random();
+    private double maxTemperatureError = 0.5;
+    private double maxHumidityError = 0.02;
 
     @Override
     public void run() {
-    
+        while (!Thread.interrupted()) {
+            temperature = room.getTemperature() + (random.nextDouble() - 0.5) * 2 * maxTemperatureError;
+            humidity = room.getHumidity() + (random.nextDouble() - 0.5) * 2 * maxHumidityError;
+
+            try {
+                Thread.sleep(updateIntervalMs);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
     }
 }
