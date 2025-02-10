5b753c1dda9c565b9cf79e0d270e24969d42d084
Arity-T <tish.mainmail@gmail.com>
2025-02-10 20:10:04 +0300

    Тест для симуляции комнаты

---
+++ lab3/src/test/java/ru/spbstu/telematics/java/RoomTests.java
@@ -0,0 +1,27 @@
+package ru.spbstu.telematics.java;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class RoomTests {
+    /* 
+     * Проверяет, что температура и влажность изменяются со временем.
+     */
+    @Test
+    public void testTemperatureAndHumidityChange() throws InterruptedException {
+        Room room = new Room();
+        double initialTemperature = room.getTemperature();
+        double initialHumidity = room.getHumidity();
+
+        Thread thread = new Thread(room);
+        thread.start();
+        Thread.sleep(5000); 
+
+        assertNotEquals(initialTemperature, room.getTemperature());
+        assertNotEquals(initialHumidity, room.getHumidity());
+
+        thread.interrupt();
+        thread.join();
+    }
+}
